ad48612542397838bf8b049d394842a40279066b
dpavlovich <dpavlovich51@gmail.com>
2023-07-21 17:21:53 +0200

    - find all sums for all ways in the tree

---
+++ src/main/java/com/dpavlovich/codewars/codewars/luxsoft/BranchCreator.java
@@ -0,0 +1,76 @@
+package com.dpavlovich.codewars.codewars.luxsoft;
+
+public interface BranchCreator {
+    void createNewBranches(NodeBuilder right, NodeBuilder left);
+
+    class Node {
+        private final int value;
+        private Node left;
+        private Node right;
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        Node(int value) {
+            this.value = value;
+            right = null;
+            left = null;
+        }
+    }
+
+    class NodeBuilder {
+
+        private Node currentNode;
+
+        public NodeBuilder(Node node) {
+            this.currentNode = node;
+        }
+
+        public static NodeBuilder root(int value, BranchCreator newBranches) {
+            Node node = new Node(value);
+            NodeBuilder nodeBuilder = new NodeBuilder(node);
+
+            NodeBuilder rightBuilder = new NodeBuilder(null);
+            NodeBuilder leftBuilder = new NodeBuilder(null);
+
+            newBranches.createNewBranches(rightBuilder, leftBuilder);
+
+            nodeBuilder.currentNode.right = rightBuilder.currentNode;
+            nodeBuilder.currentNode.left = leftBuilder.currentNode;
+
+            return nodeBuilder;
+        }
+
+        public void newNode(int value) {
+            newNode(value, (right, left) -> {
+
+            });
+        }
+
+        public void newNode(int value, BranchCreator newBranches) {
+            currentNode = new Node(value);
+
+            NodeBuilder rightBuilder = new NodeBuilder(null);
+            NodeBuilder leftBuilder = new NodeBuilder(null);
+
+            newBranches.createNewBranches(rightBuilder, leftBuilder);
+
+            currentNode.right = rightBuilder.currentNode;
+            currentNode.left = leftBuilder.currentNode;
+
+        }
+
+        public Node getParent() {
+            return currentNode;
+        }
+    }
+}
---
+++ src/main/java/com/dpavlovich/codewars/codewars/luxsoft/FindAllSumsInTreeAndCollectThemTask.java
@@ -0,0 +1,70 @@
+package com.dpavlovich.codewars.codewars.luxsoft;
+
+
+import com.dpavlovich.codewars.codewars.luxsoft.BranchCreator.Node;
+import com.dpavlovich.codewars.codewars.luxsoft.BranchCreator.NodeBuilder;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class FindAllSumsInTreeAndCollectThemTask {
+
+    public static void main(String[] args) {
+        Node root = NodeBuilder.root(1, (right, left) -> {
+                    right.newNode(2, (right1, left1) -> {
+                        right1.newNode(2, (right2, left2) -> {
+                            right2.newNode(5);
+                        });
+                        left1.newNode(4);
+                    });
+
+                    left.newNode(3);
+
+                })
+                .getParent();
+
+        show(root);
+
+        int sum = 0;
+        Collection<Integer> sums = new ArrayList<>();
+        calculateSum(root, sum, sums);
+        System.out.println(sums);
+    }
+
+    private static void calculateSum(Node node, int sum, Collection<Integer> sums) {
+        if (node == null) return;
+        sum += node.getValue();
+        if (isLeaf(node)) {
+            sums.add(sum);
+        }
+        if (node.getLeft() != null) {
+            calculateSum(node.getLeft(), sum, sums);
+        }
+        if (node.getRight() != null) {
+            calculateSum(node.getRight(), sum, sums);
+        }
+    }
+
+    private static boolean isLeaf(Node node) {
+        return node.getLeft() == null && node.getRight() == null;
+    }
+
+    private static void show(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(node.getValue());
+
+        Node right = node.getRight();
+        Node left = node.getLeft();
+
+        if (right != null) {
+            show(right);
+        }
+        if (left != null) {
+            show(left);
+        }
+    }
+
+}
